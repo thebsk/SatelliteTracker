e6f8cddbebf2cfb37c9bf07f04e7ff57d4c4b7de
burasinankarabulut <buraksinankarabulut@gmail.com>
2025-09-17 17:44:43 +0300

    bump version to 1.1 and update versionCode to 2 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.satellitetracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -66,17 +66,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.material.icons.extended)
 
     //retrofit
     implementation(libs.retrofit)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.serialization.core)
     implementation(libs.retrofit2.kotlinx.serialization.converter)
-
-    // datastore
-    implementation(libs.datastore.preferences)
-    implementation(libs.datastore)
 
     // kotlinx serialization
     implementation(libs.kotlinx.serialization.json)
